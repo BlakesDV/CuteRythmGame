b7d74e89c79f70315d7a622eb1873bf412af1a0a
Blakes <dmunoz@cocoschool.mx>
2024-05-08 08:29:30 -0600

    Game Manager Update
    Score and streak functions implementation

--- Cute Rythm Game/Assets/Scrpts/GameManager.cs
+++ Cute Rythm Game/Assets/Scrpts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,13 +10,23 @@ public class GameManager : MonoBehaviour
     public AudioSource musicSource;
     public bool startPlaying;
     public BeatDrop beatDrop;
+
     public int currentScore;
     public int scorePerNote = 100;
+    public TMP_Text scoreText;
+    public TMP_Text streakText;
+
+    public int currentStreak;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        scoreText.text = "0";
+        streakText.text = "x0";
+        currentStreak = 1;
     }
 
     // Update is called once per frame
@@ -35,11 +46,26 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("si");
-        currentScore += scorePerNote;
+
+        if(currentStreak - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+            if (multiplierThresholds[currentStreak - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentStreak++;
+            }
+        }
+        currentScore += scorePerNote * currentStreak;
+        scoreText.text = "" + currentScore;
+        streakText.text = "x" + currentStreak;
     }
 
     public void NoteMissed()
     {
         Debug.Log("no");
+        currentStreak = 1;
+        multiplierTracker = 0;
+        streakText.text = "x" + currentStreak;
     }
 }
